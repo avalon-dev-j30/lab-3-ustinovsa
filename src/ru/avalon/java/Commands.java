e1e0f275d8b8771efc0f55c18415498d2650e0b8
Daniel Alpatov <danial.alpatov@gmail.com>
2019-05-06 14:05:56 +0300

    Initial commit!

---
+++ src/ru/avalon/java/Commands.java
@@ -0,0 +1,13 @@
+package ru.avalon.java;
+
+/**
+ * Обрабатываемые приложением команды.
+ */
+public enum Commands {
+    move,
+    copy,
+    exit
+    /*
+     * TODO №8 К текущему списку команд, добавьте ещё две команды
+     */
+}
